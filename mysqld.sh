d59f665b09240a60a60434e742be50b82517003f
Mihai Csaky <mihai.csaky@gmail.com>
2016-03-28 13:34:07 +0300

    fixes

--- mysqld.sh
+++ mysqld.sh
@@ -13,14 +13,20 @@ if [ ! -d "$DATADIR/mysql" ]; then
  dpkg-reconfigure -f noninteractive  $MYSQL_SERVER
 fi
 
+
+sleep 30
+
 if [ "$CREATE_DATABASE" = "true" ]; then
 #MYSQL_DBNAME
 #MYSQL_DBUSER
 #MYSQL_DBPASSWORD
 # echo "SQL_STATEMENT" |  mysql -u root --password="${MYSQL_ROOT_PASSWORD}"
+ mysqld_safe & sleep 10s
  echo "CREATE DATABASE ${MYSQL_DBNAME};" | mysql -u root --password="${MYSQL_ROOT_PASSWORD}"
  echo "CREATE USER ${MYSQL_DBUSER}@'%' IDENTIFIED BY '${MYSQL_DBPASSWORD}';" | mysql -u root --password="${MYSQL_ROOT_PASSWORD}"
  echo "GRANT ALL ON ${MYSQL_DBNAME}.* TO ${MYSQL_DBUSER}@'%' WITH GRANT OPTION;" | mysql -u root --password="${MYSQL_ROOT_PASSWORD}"
+ killall mysqld_safe & sleep 3s
+ killall -9 mysqld_safe & sleep 2s
 fi
 
 
